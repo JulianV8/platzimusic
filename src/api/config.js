b6e397bc60d36b8fb28cadb0534a25fcb12c68bc
julianv8 <juliandjv@gmail.com>
2019-09-04 22:14:03 -0500

    Aplicacion VueJS consumiendo API de lastfm

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '25ce52eb0ce177176010a41c3abb0002',
+  secret: '38161a143c12ab345c788c2d2da2f4cc',
+  RegisteredTo: 'julianv8'
+}
+
+// Application name 	platzimusic
+// API key 	25ce52eb0ce177176010a41c3abb0002
+// Shared secret 	38161a143c12ab345c788c2d2da2f4cc
+// Registered to 	julianv8
